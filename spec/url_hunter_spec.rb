086cef2abf307436424a0d3b72cef727ced5eac8
Bryan Shelton <bryan@sheltonplace.com>
2012-04-21 16:45:40 -0500

    More tests

--- spec/url_hunter_spec.rb
+++ spec/url_hunter_spec.rb
@@ -4,4 +4,14 @@ describe UrlHunter, '#resolve' do
   it "Should require http:// or https:// prefixes" do
     lambda { UrlHunter.resolve('no_prefix') }.should raise_error(UrlHunter::Error)
   end
+
+  it "Should resolve bit.ly urls pretty well" do
+    UrlHunter.resolve('http://bit.ly/DhAoX').should eq('http://www.apple.com/')
+  end
+
+  it "Should resolve the bit.ly url for http://www.apple.com/ in one try" do
+    u = UrlHunter.new('http://bit.ly/DhAoX')
+    u.resolve.should eq('http://www.apple.com/')
+    u.tries.should eq(1)
+  end
 end
